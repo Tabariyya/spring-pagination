b19227755e36df83077bbb85ec70006a938bba1f
epsillonn <faruk.erat@vocoapp.com>
2026-08-28 02:12:05 +0500

    refactor: improve PathBuilder class by simplifying entity name handling

--- src/main/java/com/tabariyya/pagination/PathBuilders.java
+++ src/main/java/com/tabariyya/pagination/PathBuilders.java
@@ -10,9 +10,11 @@ public final class PathBuilders {
 
     public static PathBuilder<?> of(Class<?> entity) {
         try {
-            String entityName = entity.getSimpleName();
-            String instanceName = entityName.substring(0, 1).toLowerCase() + entityName.substring(1);
-            Class<?> qClass = Class.forName(entity.getPackage().getName() + ".Q" + entityName);
+            String className = entity.getSimpleName();
+            String packageName = entity.getPackage().getName();
+            String qFqn = packageName + ".Q" + className;
+            Class<?> qClass = Class.forName(qFqn);
+            String instanceName = className.substring(0, 1).toLowerCase() + className.substring(1);
             EntityPath<?> entityPath = (EntityPath<?>) qClass.getField(instanceName).get(null);
             return new PathBuilder<>(entityPath.getType(), entityPath.getMetadata());
         } catch (Throwable e) {
