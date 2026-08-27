d88eb19200ced4b8fbcfb45ddde4ebc6f10c0d63
epsillonn <faruk.erat@vocoapp.com>
2026-08-28 02:24:57 +0500

    refactor: simplify PathBuilders class by enhancing the of method and adding decapitalize utility

--- src/main/java/com/tabariyya/pagination/PathBuilders.java
+++ src/main/java/com/tabariyya/pagination/PathBuilders.java
@@ -1,6 +1,5 @@
 package com.tabariyya.pagination;
 
-import com.querydsl.core.types.EntityPath;
 import com.querydsl.core.types.dsl.PathBuilder;
 
 public final class PathBuilders {
@@ -8,17 +7,11 @@ public final class PathBuilders {
     private PathBuilders() {
     }
 
-    public static PathBuilder<?> of(Class<?> entity) {
-        try {
-            String className = entity.getSimpleName();
-            String packageName = entity.getPackage().getName();
-            String qFqn = packageName + ".Q" + className;
-            Class<?> qClass = Class.forName(qFqn);
-            String instanceName = className.substring(0, 1).toLowerCase() + className.substring(1);
-            EntityPath<?> entityPath = (EntityPath<?>) qClass.getField(instanceName).get(null);
-            return new PathBuilder<>(entityPath.getType(), entityPath.getMetadata());
-        } catch (Throwable e) {
-            throw new GenericQueryDslException(e);
-        }
+    public static <T> PathBuilder<T> of(Class<T> entity) {
+        return  new PathBuilder<>(entity, decapitalize(entity.getSimpleName()));
+    }
+
+    private static String decapitalize(String name) {
+        return name.isEmpty() ? name : Character.toLowerCase(name.charAt(0)) + name.substring(1);
     }
 }
